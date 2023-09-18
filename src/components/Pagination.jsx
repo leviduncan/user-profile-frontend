3c4643a7a9c7c0d1f428367b408dbe29e57da59a
darrin@darrinduncan.com <darrin@darrinduncan.com>
2023-09-17 23:25:16 -0400

    Fix: Search logic

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 const Pagination = (props) => {
-  const {usersPerPage, totalUsers, paginate} = props
-  const pageNumbers = []
+  const { usersPerPage, totalUsers, paginate } = props;
+  const pageNumbers = [];
 
-  for(let i = 1; i <= Math.ceil(totalUsers / usersPerPage); i++) {
-    pageNumbers.push(i)
+  for (let i = 1; i <= Math.ceil(totalUsers / usersPerPage); i++) {
+    pageNumbers.push(i);
   }
 
   return (
     <div className="d-flex justify-content-center mt-3">
-            <div className="nav">
-                <ul className="pagination">
-                    {pageNumbers.map(number => (
-                        <li key={number} className="page-item">
-                            <span onClick={() => paginate(number)} className="page-link">{number}</span>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-  )
-}
+      <div className="nav">
+        <ul className="pagination">
+          {pageNumbers.map((number) => (
+            <li key={number} className="page-item">
+              <button
+                onClick={() => paginate(number)}
+                className="page-link"
+                aria-label={`Go to page ${number}`}
+              >
+                {number}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Pagination
+export default Pagination;
--- src/components/ShowUsers.jsx
+++ src/components/ShowUsers.jsx
@@ -1,75 +1,72 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import UserCard from './UserCard'
-import Title from './Title'
-import Loader from './Loader'
-import Pagination from './Pagination'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import UserCard from './UserCard';
+import Title from './Title';
+import Loader from './Loader';
+import Pagination from './Pagination';
 
 const ShowUsers = () => {
+  const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState('');
+  const [filteredUsers, setFilteredUsers] = useState([]); // New state for filtered users
+  const [currentPage, setCurrentPage] = useState(1);
+  const [usersPerPage] = useState(12);
+  const page = 'show';
 
-    const [users, setUsers] = useState([])
-    const [search, setSearch] = useState('')
-    const [currentPage, setCurrentPage] = useState(1)
-    const [usersPerPage] = useState(12)
-    const page = "show"
-    const handleSearch = (e) => {
-        setSearch(e.target.value);
-      };
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
 
-    useEffect(() => {
-        axios
-            .get('https://userprofilebackend.cyclic.app/api/')
-            .then((res) => {
-                setUsers(res.data)
-            })
-            .catch((err) => {
-                console.log('Error occurred while fetching user data:', err)
-            })
-    }, [])
+  useEffect(() => {
+    axios
+      .get('https://userprofilebackend.cyclic.app/api/')
+      .then((res) => {
+        setUsers(res.data);
+        setFilteredUsers(res.data); // Initialize filteredUsers with all users
+      })
+      .catch((err) => {
+        console.log('Error occurred while fetching user data:', err);
+      });
+  }, []);
 
-    const indexOfLastUser = currentPage * usersPerPage
-    const indexOfFirstUser = indexOfLastUser - usersPerPage
-    const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser)
-    const paginate = (pageNumber) => setCurrentPage(pageNumber)
-    const userList =
-        users.length === 0
-            ? <Loader />
-            : currentUsers.filter((user) => {
-                const searchTerm = search.toLowerCase()
-                if (searchTerm === "") {
-                    return true
-                } else if (
-                    user.fname.toLowerCase().includes(searchTerm) || 
-                    user.lname.toLowerCase().includes(searchTerm) || 
-                    user.title.toLowerCase().includes(searchTerm)
-                ) {
-                    return true
-                }
-                return false
-            }).map((user) => (
-            <UserCard user={user} key={user.id} />
-            ))
+  useEffect(() => {
+    // When the search input changes, filter users based on the search term
+    const searchTerm = search.toLowerCase();
+    const filtered = users.filter(
+      (user) =>
+        user.fname.toLowerCase().includes(searchTerm) ||
+        user.lname.toLowerCase().includes(searchTerm) ||
+        user.title.toLowerCase().includes(searchTerm)
+    );
+    setFilteredUsers(filtered);
+    setCurrentPage(1); // Reset to the first page when search changes
+  }, [search, users]);
 
-    const userAmt = users.length
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+  const currentUsers = filteredUsers.slice(indexOfFirstUser, indexOfLastUser);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
+  const userList =
+    filteredUsers.length === 0 ? (
+      <Loader />
+    ) : (
+      currentUsers.map((user) => <UserCard user={user} key={user.id} />)
+    );
 
-    return (
-        <div className="ShowUsers">
-            <div className="container">
-                <div className="row">
-                    <Title page={page} userAmt={userAmt} handleSearch={handleSearch}/>
-                </div>
-                <div className="list">
-                    {userList}
-                </div>
-                <Pagination 
-                    usersPerPage={usersPerPage}
-                    totalUsers={userAmt}
-                    paginate={paginate}
-                />
-            </div>
+  const userAmt = filteredUsers.length;
+
+  return (
+    <div className="ShowUsers">
+      <div className="container">
+        <div className="row">
+          <Title page={page} userAmt={userAmt} handleSearch={handleSearch} />
         </div>
-    )
-}
+        <div className="list">{userList}</div>
+        <Pagination usersPerPage={usersPerPage} totalUsers={userAmt} paginate={paginate} />
+      </div>
+    </div>
+  );
+};
 
-export default ShowUsers
+export default ShowUsers;
